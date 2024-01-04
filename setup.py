30505e95f080490d81a3e1cb58bea810812957ad
Ryan Ziegler <rzig408@gmail.com>
2024-01-04 15:49:00 -0600

    prepare for version upgrade

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="bettertags",
-    version="1.0.3",
+    version="1.1.0",
     author="ryan",
     author_email="ryan@ziegler.lol",
     url="https://github.com/rzig/bettertags",
     install_requires=["markdown", "mkdocs"],
-    description="mkdocs-material tag plugin with free enhanced versions of some insiders features",
+    description="mkdocs-material tag plugin with FREE enhanced versions of all insiders features",
     packages=find_packages(),
     zip_safe=False,
     entry_points={
